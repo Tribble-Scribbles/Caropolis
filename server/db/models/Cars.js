92fec89aa4773dfd827c2053271bcf93135fb228
Jason Bell <jason.jbell@yahoo.com>
2022-01-11 09:25:57 -0800

    updated user model and created car model

---
+++ server/db/models/Cars.js
@@ -0,0 +1,57 @@
+const { INTEGER } = require('sequelize')
+const Sequelize = require('sequelize')
+const db = require('../db')
+const { STRING, INTEGER } = Sequelize.DataTypes
+
+const Car = db.define('cars', {
+  year: {
+    type: INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 1900,
+      max: 2023
+    }
+  },
+  make : {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  model: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: DECIMAL,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      isUrl: true
+    }
+  },
+  color: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  origin: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+})
+
+module.exports = Car
