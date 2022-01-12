4e292a26a323e1851b11a380e3abbda2f0342d9a
Jason Bell <jason.jbell@yahoo.com>
2022-01-11 16:25:58 -0800

    changesss

--- script/seed.js
+++ script/seed.js
@@ -4,10 +4,6 @@ const axios = require('axios')
 const {db, models: {User, Car} } = require('../server/db')
 const token = 'ZrQEPSkKc3VuZzk2a2ltQGdtYWlsLmNvbQ=='
 
-const {
-  db,
-  models: { User, Car },
-} = require("../server/db");
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
@@ -19,7 +15,7 @@ const {
 
 async function createCars(){
   const {data: carsData} = await axios.get('https://auto.dev/api/listings?apikey=ZrQEPSkKc3VuZzk2a2ltQGdtYWlsLmNvbQ==&make=Make&model=Model%203&category=supercar&radius=5000&page=1')
-  const cars = carsData.records.map(car => {
+  const filtered = carsData.records.map(car => {
     let carObj = {
       vin: car.vin,
       trim: car.trim,
@@ -43,7 +39,7 @@ async function createCars(){
     return true
   })
 
-  return cars;
+  return filtered;
 }
 
 async function seed() {
@@ -67,31 +63,7 @@ async function seed() {
     })
   ])
 
-  const cars = await Promise.all([
-    Car.create({
-      year: 2007,
-      make: "Porshe",
-      model: "911",
-      price: 73260.0,
-      imageUrl:
-        "https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/images/07q1/267362/2007-porsche-911-targa-4s-photo-8803-s-original.jpg?fill=2:1&resize=480:*",
-      color: "black",
-      origin: "Germany",
-      qty: 4,
-
-    }),
-    Car.create({
-      year: 2005,
-      make: "Lamborghini",
-      model: "Gallardo",
-      price: 176000.0,
-      imageUrl:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mecum.com%2Flots%2FCA0819-389941%2F2005-lamborghini-gallardo%2F&psig=AOvVaw1Em3I_SLIWBo_pTbHQaPC6&ust=1642019682145000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjRppnGqvUCFQAAAAAdAAAAABAD",
-      color: "yellow",
-      origin: "Italy",
-      qty: 3,
-    }),
-  ])
+  
   const cars = await createCars();
 
   await Promise.all(cars.map(car => {
@@ -108,32 +80,9 @@ async function seed() {
     },
     cars
   }
-=======
-    Car.create({
-      year: 2021,
-      make: "Mercedes",
-      model: "GT63",
-      price: 73260.0,
-      imageUrl:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcarbuzz.com%2Fcars%2Fmercedes-benz%2Famg-gt-63&psig=AOvVaw2_P31zAJyqFdjZuX90n6y7&ust=1642019916264000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMiS94LHqvUCFQAAAAAdAAAAABAD",
-      color: "white",
-      origin: "Germany",
-    }),
-  ]);
-
-  console.log(`seeded ${users.length} users`);
-  console.log(`seeded ${cars.length} cars`);
-  console.log(`seeded successfully`);
-  return {
-    users: {
-      cody: users[0],
-      murphy: users[1],
-    },
-    cars,
-  };
 }
 
-/*
+/*npm run
  We've separated the `seed` function from the `runSeed` function.
  This way we can isolate the error handling and exit trapping.
  The `seed` function is concerned only with modifying the database.
