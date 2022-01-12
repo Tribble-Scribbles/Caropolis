9eca103693ac86a61790fdd56659ba9b8625b7ee
Sung <sung96kim@gmail.com>
2022-01-12 15:43:24 -0500

    ADD dependencies and refactor home/navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,113 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {logout} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id
-  }
-}
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+}))
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
+export default function Navbar() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => {
+    return {
+      isLoggedIn: !!state.auth.id
     }
-  }
-}
+  })
 
-export default connect(mapState, mapDispatch)(Navbar)
+  return(
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+          <Toolbar className={classes.toolbar}>
+            <Link underline="none" variant="button" color="inherit" href="/home" className={classes.toolbarTitle}>
+              <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                Caropolis
+              </Typography>
+            </Link>
+
+            <nav>
+              <Link variant="button" color="textPrimary" href="/home" className={classes.link}>
+                Home
+              </Link>
+              <Link variant="button" color="textPrimary" href="/listings" className={classes.link}>
+                Listings
+              </Link>
+              <Link variant="button" color="textPrimary" href="/account" className={classes.link}>
+                Account
+              </Link>
+            </nav>
+            {/* {
+              isLoggedIn ? (
+                <Button href="/" color="primary" variant="outlined" className={classes.link} onClick={() => {dispatch(logout())}}>
+                  Logout
+                  {console.log(isLoggedIn)}
+                </Button>
+              ) : (
+                <Button href="/login" color="primary" variant="outlined" className={classes.link}>
+                  Login
+                  {console.log(isLoggedIn)}
+                </Button>
+              )
+            } */}
+            <Button href="/" color="primary" variant="outlined" className={classes.link} onClick={() => {dispatch(logout())}}>
+              Logout
+            </Button>
+            <Button href="/login" color="primary" variant="outlined" className={classes.link}>
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+    </React.Fragment>
+  )
+}
+/* <nav>
+  {isLoggedIn ? (
+    <div>
+      <Link to="/home">Home</Link>
+      <a href="#" onClick={handleClick}>
+        Logout
+      </a>
+    </div>
+  ) : (
+    <div>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Sign Up</Link>
+    </div>
+  )}
+</nav>
+<hr /> */
+
+// const mapDispatch = dispatch => {
+//   return {
+//     handleClick() {
+//       dispatch(logout())
+//     }
+//   }
+// }
