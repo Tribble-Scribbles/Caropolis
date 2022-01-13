d9b1feb1a36927af04b947f789934b9d071b1779
Sung <sung96kim@gmail.com>
2022-01-13 03:20:27 -0500

    REVERT routes refactor since it is missing withRouter

--- client/Routes.js
+++ client/Routes.js
@@ -1,36 +1,89 @@
-import React from 'react'
-import { Route, Switch, Redirect} from 'react-router-dom'
+import React, { Component } from 'react'
+import { withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
 import Home from './components/Home';
 import SingleCar from './components/SingleCar';
 import AllCars from './components/AllCars';
+import { connect } from 'react-redux';
+import { me } from './store'
 
-const Routes = () => {
-  // const isLoggedIn = useSelector((state) => {
-  //   return {
-  //     loginStatus: state.auth.loginStatus
-  //   }
-  // })
+class Routes extends Component {
+  componentDidMount() {
+    this.props.loadInitialData()
+  }
 
-  return (
-    <div>
-      {/* {isLoggedIn ? (
-        <Switch>
-          <Route path="/home" component={Home}/>
-          <Redirect to="/home" component={Home}/>
-        </Switch>
-      ) : ( */}
-        <Switch>
-          <Route exact path='/' exact component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/login" component={LogIn} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path='/cars' component={AllCars} />
-          <Route exact path="/cars/:id" component={SingleCar} />
-        </Switch>
-      {/* )} */}
-    </div>
-  )
+  render() {
+    const {isLoggedIn} = this.props
+
+    return (
+      <div>
+        {/* {isLoggedIn ? (
+          <Switch>
+            <Route path="/home" component={Home} />
+          </Switch>
+        ) : ( */}
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route exact path='/' exact component={LogIn} />
+            <Route path="/login" component={LogIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route exact path='/cars' component={AllCars} />
+            <Route exact path="/cars/:id" component={SingleCar} />
+
+          </Switch>
+
+      </div>
+    )
+  }
 }
-export default Routes;
+
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.auth.id
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData() {
+      dispatch(me())
+    }
+  }
+}
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapState, mapDispatch)(Routes))
+
+
+// Refactored hook version but it is missing withRouter implementation
+
+// const Routes = () => {
+//   // const isLoggedIn = useSelector((state) => {
+//   //   return {
+//   //     loginStatus: state.auth.loginStatus
+//   //   }
+//   // })
+
+//   return (
+//     <div>
+//       {/* {isLoggedIn ? (
+//         <Switch>
+//           <Route path="/home" component={Home}/>
+//           <Redirect to="/home" component={Home}/>
+//         </Switch>
+//       ) : ( */}
+//         <Switch>
+//           <Route exact path='/' exact component={Home} />
+//           <Route exact path="/home" component={Home} />
+//           <Route exact path="/login" component={LogIn} />
+//           <Route exact path="/signup" component={SignUp} />
+//           <Route exact path='/cars' component={AllCars} />
+//           <Route exact path="/cars/:id" component={SingleCar} />
+//         </Switch>
+//       {/* )} */}
+//     </div>
+//   )
+// }
+// export default Routes;
