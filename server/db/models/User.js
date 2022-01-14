88bf51642e855f7d122a6d983c474c15d66f33d9
Eddie Shev <eddieshev@gmail.com>
2022-01-14 15:44:01 -0500

    added isUnique validator on email user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,13 +1,13 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 // const axios = require('axios');
-const { STRING } = Sequelize.DataTypes
+const { STRING } = Sequelize.DataTypes;
 
 const SALT_ROUNDS = 5;
 
-const User = db.define('users', {
+const User = db.define("users", {
   password: {
     type: STRING,
   },
@@ -15,86 +15,88 @@ const User = db.define('users', {
     type: STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   lastName: {
     type: STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
   imageUrl: {
     type: STRING,
     allowNull: false,
     validate: {
-      isUrl: true
+      isUrl: true,
     },
-    defaultValue: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0JTIwd2l0aCUyMGdsYXNzZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    defaultValue:
+      "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2F0JTIwd2l0aCUyMGdsYXNzZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
   },
   email: {
     type: STRING,
     allowNull: false,
     validate: {
+      isUnique: true,
       notEmpty: true,
-      isEmail: true
-    }
-  }
-})
+      isEmail: true,
+    },
+  },
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
-User.prototype.correctPassword = function(candidatePwd) {
+User.prototype.correctPassword = function (candidatePwd) {
   //we need to compare the plain version to an encrypted version of the password
   return bcrypt.compare(candidatePwd, this.password);
-}
+};
 
-User.prototype.generateToken = function() {
-  return jwt.sign({id: this.id}, process.env.JWT)
-}
+User.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
 
 /**
  * classMethods
  */
-User.authenticate = async function({ email, password }){
-    const user = await this.findOne({where: { email }})
-    if (!user || !(await user.correctPassword(password))) {
-      const error = Error('Incorrect username/password');
-      error.status = 401;
-      throw error;
-    }
-    return user.generateToken();
+User.authenticate = async function ({ email, password }) {
+  const user = await this.findOne({ where: { email } });
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error("Incorrect username/password");
+    error.status = 401;
+    throw error;
+  }
+  return user.generateToken();
 };
 
-User.findByToken = async function(token) {
+User.findByToken = async function (token) {
   try {
-    const {id} = await jwt.verify(token, process.env.JWT)
-    const user = User.findByPk(id)
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = User.findByPk(id);
     if (!user) {
-      throw 'nooo'
+      throw "nooo";
     }
-    return user
+    return user;
   } catch (ex) {
-    const error = Error('bad token')
-    error.status = 401
-    throw error
+    const error = Error("bad token");
+    error.status = 401;
+    throw error;
   }
-}
+};
 
 /**
  * hooks
  */
-const hashPassword = async(user) => {
+const hashPassword = async (user) => {
   //in case the password has been changed, we want to encrypt it with bcrypt
-  if (user.changed('password')) {
+  if (user.changed("password")) {
     user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
   }
-}
+};
 
-User.beforeCreate(hashPassword)
-User.beforeUpdate(hashPassword)
-User.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
+User.beforeCreate(hashPassword);
+User.beforeUpdate(hashPassword);
+User.beforeBulkCreate((users) => Promise.all(users.map(hashPassword)));
