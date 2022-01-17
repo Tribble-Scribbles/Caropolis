416f84bb89a287b8b5c1c4523b972466dfa295a7
Sung <sung96kim@gmail.com>
2022-01-17 16:09:18 -0500

    ADD singlecar component

--- client/components/SingleCar.js
+++ client/components/SingleCar.js
@@ -1,46 +1,109 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCar } from "../store/car";
-import { useParams } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
-const SingleCar = () => {
+import { makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import { Grid } from "@material-ui/core";
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const useStyles = makeStyles((theme) => ({
+  modalOuterContainer: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "center"
+  },
+  image: {
+    flexGrow: 1,
+    padding: "10px"
+  },
+  text: {
+    flexGrow: 2,
+    padding: "10px"
+  },
+  align:{
+    textAlign: "center"
+  }
+}));
+
+export default function SingleCar(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const { carId } = props;
+
   const dispatch = useDispatch();
-  const { id } = useParams();
+  const history = useHistory();
   const { car } = useSelector((state) => {
     return {
       car: state.car,
     };
   });
-  const dealerName = "California Motors Direct"
-  const state = "CA"
-  const condition = 'used'
 
-  useEffect(() => {
-    dispatch(fetchCar(id));
-  }, []);
-  
-  console.log(car)
+  const handleClickOpen = () => {
+    history.push(carId)
+    dispatch(fetchCar(carId));
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div>
-      <div id="single-car-info">
-        <h3>{car.year} {car.make} {car.model}</h3>
-        <h3>{car.price}</h3>
-        <img className="single-car" src={car.imageUrl} />
-      </div>
-      <div id="right-of-car">
-        <h3>Sold by: {dealerName}</h3>
-        <h4>Located in: {car.city}, {state}</h4>
-        <h4>Condition: {condition}</h4>
-        <h4>Mileage: {car.mileage}</h4>
-        <h4>Body Style: {car.bodyType}</h4>
-        <h4>Exterior color: {car.color}</h4>
-        <h4>VIN: {car.vin}</h4>
-        <h4>Trim: {car.trim}</h4>
-        <button>Add To Cart</button>
-      </div>
-    </div>
-  );
-};
+    <React.Fragment>
+      <Button variant="text" color="primary" size="small" onClick={handleClickOpen}>
+        View
+      </Button>
+      <Dialog
+        fullWidth={true}
+        maxWidth="md"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="max-width-dialog-title"
+        className={classes.align}
+      >
+        <DialogTitle id="max-width-dialog-title" className={classes.align}>{car.year} {car.make} {car.model}</DialogTitle>
 
-export default SingleCar;
+        <DialogContent dividers className={classes.modalOuterContainer}>
+            <Grid item className={classes.image} >
+              <img src={car.imageUrl} style={{height: "500px", width: "90%", padding: "10px"}}/>
+            </Grid>
+            <Grid item className={classes.text}>
+              <DialogContentText>
+                {`
+                  Sold by: ${car.dealerName} |
+                  Located in: ${car.city}, ${car.state} |
+                  VIN: ${car.vin} |
+                  Condition: ${car.condition} |
+                  `}
+              </DialogContentText>
+              <DialogContentText>
+                {`
+                  Mileage: ${car.mileage} |
+                  Trim: ${car.trim} |
+                  Body Style: ${car.bodyType} |
+                  Exterior color: ${car.color} |
+                `}
+              </DialogContentText>
+              <DialogContent>
+                {`
+                  Price: $${car.price}
+                `}
+              </DialogContent>
+            </Grid>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+    )
+  }
