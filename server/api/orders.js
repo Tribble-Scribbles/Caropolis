3a11d9d3596f2b31fca3b494885c48cffc078667
Sung <sung96kim@gmail.com>
2022-01-18 16:03:10 -0500

    ADD orders post/get route

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,10 +1,34 @@
 const router = require('express').Router()
-const { models: { Order }} = require('../db')
+const { models: { Car, Order, User }} = require('../db')
 
 router.get('/', async (req, res, next) => {
   try {
-
+    const orders = await Order.findAll()
+    res.send(orders);
   } catch (error) {
-
+    next(error)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    //needs to receive userId, and the cart which is an array
+    const id = req.body.userId;
+    const cart = req.body.cart;
+    const order = await Order.create({})
+    const user = await User.findByPk(id)
+
+    user.addOrder(order)
+
+    cart.forEach( async (car) => {
+      const foundCar = await Car.findByPk(car.id)
+      foundCar.setOrder(order)
+    })
+
+    res.status(201).send(order);
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router;
--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -2,9 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 const { STRING, INTEGER } = Sequelize.DataTypes
 
-const Order = db.define('orders', {
-
-
-})
+const Order = db.define('orders', {})
 
 module.exports = Order;
